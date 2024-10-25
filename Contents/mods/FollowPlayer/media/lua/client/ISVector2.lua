14a657159431419101c049e05b5ae3a77c575533
Stefano Scalfari <vorshim92@gmail.com>
2024-10-25 02:13:07 +0200

    some improvements to isvector2 class

--- Contents/mods/FollowPlayer/media/lua/client/ISVector2.lua
+++ Contents/mods/FollowPlayer/media/lua/client/ISVector2.lua
@@ -6,34 +6,36 @@ local ISVector2 = {}
 
 
 ---comment
----@param x number
----@param y number
----@return ISVector2
-function ISVector2.newVector(x,y)
-    local player = getPlayer()
+---@param player IsoPlayer
+---@return Vector2
+function ISVector2.lastAngle2Vector(player)
     if not player then return nil end
     -- workaround using:
     -- public Vector2 getLastAngle() {
     --     return this.lastAngle;
     --  }
     -- of isoPlayer to create javaobject vector from the player
-    local tempVector = player:getLastAngle()
-    if not tempVector then return nil end
-    local newVector = tempVector:clone()
-    newVector:set(x,y)
-    return newVector
+    local lastAngleVector = player:getLastAngle()
+    if not lastAngleVector then return nil end
+    return lastAngleVector
 end
 
 ---comment
----@param isoplayer any
----@return ISVector2
+---@param isoplayer IsoPlayer
+---@return Vector2
 function ISVector2.isoPlayer2Vector(isoplayer)
     if not isoplayer then return nil end
     if not isoplayer.getX then return nil end
     local x = isoplayer:getX()
     local y = isoplayer:getY()
     if not x or not y then return nil end
-    return ISVector2.newVector(x,y)
+    return ISVector2.newVector(x, y)
+end
+
+function ISVector2.newVector(x, y)
+    local newVector = Vector2.new()
+    newVector:set(x, y)
+    return newVector
 end
 
 return ISVector2
