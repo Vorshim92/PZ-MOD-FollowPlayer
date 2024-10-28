e7dc980c19053002991fd9039f24f7160e94257a
Stefano Scalfari <vorshim92@gmail.com>
2024-10-28 13:24:24 +0100

    working. need to clean the code

--- Contents/mods/FollowPlayer/media/lua/client/ISFollowPlayer.lua
+++ Contents/mods/FollowPlayer/media/lua/client/ISFollowPlayer.lua
@@ -87,14 +87,7 @@ function ISFollowPlayer.onFillContext(player, context, worldobjects, test)
     local playerObj = getSpecificPlayer(player)
     local followerCount = 0
     local followers = {}
-    -- for i, obj in ipairs(worldobjects) do
-    --     if instanceof(obj, "IsoPlayer") then
-    --             if ISFollowPlayer.CanFollowPlayer(playerObj, clickedPlayer) and not followers[clickedPlayer:getUsername()] then
-    --             followers[obj:getUsername()] = obj
-    --             followerCount = followerCount + 1
-    --         end
-    --     end
-    -- end
+
     for _, v in ipairs(worldobjects) do
         if v:getSquare() then
             -- help detecting a player by checking nearby squares
@@ -156,86 +149,3 @@ end
 
 
 Events.OnFillWorldObjectContextMenu.Add(ISFollowPlayer.onFillContext)
-
-
---per calcolare la distanza dal player:
--- if math.abs(playerObj:getX() - clickedPlayer:getX()) > 2 or math.abs(playerObj:getY() - clickedPlayer:getY()) > 2 then
--- end
--- oppure 
--- local distanceSq = playerObj:getDistanceSq(clickedPlayer)
-
--- per la creazione del tooltip
--- local tooltip = ISWorldObjectContextMenu.addToolTip();
--- 			option.notAvailable = true;
--- 			tooltip.description = getText("ContextMenu_GetCloser", clickedPlayer:getDisplayName());
--- 			option.toolTip = tooltip;
-
-
--- alternativa al for worldobjects che ho fatto io:
-
--- for _, v in ipairs(worldobjects) do
---     if v:getSquare() then
---         -- help detecting a player by checking nearby squares
---         for x = v:getSquare():getX() - 1, v:getSquare():getX() + 1 do
---             for y = v:getSquare():getY() - 1, v:getSquare():getY() + 1 do
---                 local sq = getCell():getGridSquare(x, y, v:getSquare():getZ())
---                 if sq then
---                     for i = 0, sq:getMovingObjects():size() - 1 do
---                         local clickedPlayer = sq:getMovingObjects():get(i)
-
---                         if TweakWorldObjectContextMenu.CanTradeWith(character, clickedPlayer) and not traders[clickedPlayer:getUsername()] then
---                             traders[clickedPlayer:getUsername()] = clickedPlayer
-
---                             local optionTrade = tradeSubMenu:addOption(getText('ContextMenu_Stranger'), worldobjects, ISWorldObjectContextMenu.onTrade, character, clickedPlayer)
---                             if (math.abs(character:getX() - clickedPlayer:getX()) > 2 or math.abs(character:getY() - clickedPlayer:getY()) > 2) then
---                                 local tooltip = ISWorldObjectContextMenu.addToolTip()
---                                 optionTrade.notAvailable = true
---                                 tooltip.description = getText("ContextMenu_GetCloserToTrade", getText('ContextMenu_Stranger'))
---                                 optionTrade.toolTip = tooltip
---                             end
---                         end
-
---                         if TweakWorldObjectContextMenu.CanHealPlayer(character, clickedPlayer) and not patients[clickedPlayer:getUsername()] then
---                             patients[clickedPlayer:getUsername()] = clickedPlayer
-
---                             local optionMedicalCheck = medicalCheckSubMenu:addOption(getText('ContextMenu_Stranger'), worldobjects, ISWorldObjectContextMenu.onMedicalCheck, character, clickedPlayer)
---                             if (math.abs(character:getX() - clickedPlayer:getX()) > 2 or math.abs(character:getY() - clickedPlayer:getY()) > 2) then
---                                 local tooltip = ISWorldObjectContextMenu.addToolTip()
---                                 optionMedicalCheck.notAvailable = true
---                                 tooltip.description = getText("ContextMenu_GetCloser", getText('ContextMenu_Stranger'))
---                                 optionMedicalCheck.toolTip = tooltip
---                             end
---                         end
---                     end
---                 end
---             end
---         end
---     end
--- end
-
-
-
-
-
--- option:setValue(selected)
---   getDebugOptions():save()
-
-
--- for debugconsole in game
--- vorshim = function ()
---     local options = getDebugOptions()
---     if options then
---         for i=1,options:getOptionCount() do
---             local option = options:getOptionByIndex(i-1)
---             print(option:getName())
---             if option:getName() == "Multiplayer.Debug.Follow.Player" then
---                 print("Value before: " .. option:getValue())
---                 option:setValue(true)
---                 print("Value after: " .. option:getValue())
---                 getDebugOptions():save()
---                 print("COMPLIMENTI VORSHIM")
---                 break
---             end
---         end
---     end
--- end
--- Contents/mods/FollowPlayer/media/lua/client/TimedActions/ISFollowPlayerTimedAction.lua
+++ Contents/mods/FollowPlayer/media/lua/client/TimedActions/ISFollowPlayerTimedAction.lua
@@ -14,41 +14,53 @@ function ISFollowToTimedAction:isValid()
 end
 
 function ISFollowToTimedAction:update()
-    -- print(self.locationX .. " x " .. self.locationY)
-    if instanceof(self.character, "IsoPlayer") and
-            (self.character:pressedMovement(false) or self.character:pressedCancelAction()) then
+    if (self.character:pressedMovement(false) or self.character:pressedCancelAction()) then
         self:forceStop()
         return
     end
 
-    self.result = self.character:getPathFindBehavior2():update(); --??
-
+    
+    local distanceSq = self.character:getDistanceSq(self.clickedplayer)
+    if distanceSq <= 100.0 then
+        if distanceSq > 25.0 then -- 5^2 = 25
+            self.character:setRunning(true)
+            self.character:setSprinting(true)
+        elseif distanceSq > 6.25 then -- 2.5^2 = 6.25
+            self.character:setRunning(true)
+        elseif distanceSq < 1.5625 then -- 1.25^2 = 1.5625
+            self.character:setRunning(false)
+            self.character:setSprinting(false)
+        end
+    -- else
+    --     -- Handle the case when the target is too far
+    end
+    
+    self.result = self.character:getPathFindBehavior2():update();
     if self.result == BehaviorResult.Failed then -- this stop the action if the character keeps trying to touch the clickedplayer, need to remove it? or we just need to change the behaviour of the path to let the charecter going to -1x -1y of the clickedplayer so that the character doesn't keep trying to touch the clickedplayer?
-        print ("ISFollowToTimedAction: pathfind failed");
-        self:forceStop();
-        return;
+        -- print ("ISFollowToTimedAction: pathfind failed");
+        -- self:forceStop();
+        -- return;
     end
 
     if self.result == BehaviorResult.Working then
         --qui possiamo aggiornare il pathindex? con add node "pathNextIsSet" maybe?
-        print ("ISFollowToTimedAction: pathfind working");
-        return;
+        -- print ("ISFollowToTimedAction: pathfind working");
     end
 
-    -- if self.character:getDistanceSq(self.clickedplayer) == 1 then
-    --     self:forceComplete();
-    --     return
-    -- end
+    if distanceSq == 1 then
+        -- print("ISFollowToTimedAction: I'm at 1 tile from the clickedplayer");
+        -- self:forceComplete();
+        -- return
+    end
 
     -- need to recalcolate path if the clickedplayer moved
-    local x2,y2 = self.clickedplayer:getX(), self.clickedplayer:getY()
-    if x2 ~= self.locationX or y2 ~= self.locationY then
-        --print ("ISFollowToTimedAction: recalculating path")
-        self.locationX = x2
-        self.locationY = y2
-        self.character:pathToCharacter(self.clickedplayer);
-        
+    local square = self.clickedplayer:getSquare()
+
+    if square ~= self.square then
+        self.character:pathToLocation(square:getX()-1, square:getY()-1, square:getZ());
+        self.square = square
     end
+        
 
     if self.additionalTest ~= nil then
        if self.additionalTest(self.additionalContext) then
@@ -57,16 +69,16 @@ function ISFollowToTimedAction:update()
             return
        end
     end
-    -- if self.result == BehaviorResult.Succeeded then
-	-- 	--print ("ISFollowToTimedAction: hit complete");
-    --     self:forceComplete();
-    -- end
+    if self.result == BehaviorResult.Succeeded then
+		-- print ("ISFollowToTimedAction: hit complete");
+        -- self:forceComplete();
+    end
 
 end
 
 function ISFollowToTimedAction:start()
     --print ("ISFollowToTimedAction: Calling pathfind method.");
-    self.character:pathToCharacter(self.clickedplayer);
+    self.character:pathToLocation(self.square:getX()-1, self.square:getY()-1, self.square:getZ());
     --self.action:Pathfind(getPlayer(), self.location:getX(), self.location:getY(), self.location:getZ());
 end
 
@@ -113,8 +125,7 @@ function ISFollowToTimedAction:new (character, clickedplayer, additionalTest, ad
     o.stopOnRun = false;
     o.maxTime = -1;
     o.clickedplayer = clickedplayer
-    o.locationX = clickedplayer:getX()
-    o.locationY = clickedplayer:getY()
+    o.square = clickedplayer:getSquare()
     o.pathIndex = 0;
     o.additionalTest = additionalTest;
     o.additionalContext = additionalContext;
